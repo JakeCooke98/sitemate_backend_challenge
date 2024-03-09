4c0b47afe57104810ff33ece5611b27d1b07595a
Jake Cooke <cooke.jake98@gmail.com>
2024-03-09 13:26:15 +1100

    Add 4 methods for our REST API (read, create, update, delete)

--- src/rest-client.js
+++ src/rest-client.js
@@ -8,6 +8,42 @@ class RestClient {
             headers: { 'Content-Type': 'application/json' },
         });
     }
+
+    async read(url) {
+        try {
+            const response = await this.client.get(url);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async create(url, data) {
+        try {
+            const response = await this.client.post(url, data);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async update(url, data) {
+        try {
+            const response = await this.client.put(url, data);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async delete(url) {
+        try {
+            const response = await this.client.delete(url);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 
 module.exports = RestClient;
